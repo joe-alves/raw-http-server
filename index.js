087b5d51d68130ac3f8a9ec6aa37695cfabe3c3e
Joe Alves <jmalves@appacademy.io>
2020-10-12 16:55:55 -0700

    hello route

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const http = require("http");
+const fs = require("fs");
 const server = http.createServer();
 
 const characters = require("./data-resources/smash-ult-chars");
@@ -9,28 +10,38 @@ myButton.addEventListener("click", () => {});
 */
 
 const randomizedResponseDelay = (dataToWrite, res) => {
-    const randomNumberAtLeastOneSecond = 1000 + Math.floor(Math.random() * 3000);
-    setTimeout(() => {
-        res.write(dataToWrite);
-        res.end();
-    }, randomNumberAtLeastOneSecond);
+  const randomNumberAtLeastOneSecond = 1000 + Math.floor(Math.random() * 3000);
+  setTimeout(() => {
+    res.write(dataToWrite);
+    res.end();
+  }, randomNumberAtLeastOneSecond);
 };
 
 server.on("request", (req, res) => {
   // On HTTP request.
   if (req.url === "/characters") {
-      randomizedResponseDelay(JSON.stringify(characters, null, 4), res);
-  } else if (req.url.length === 2) { "/a /b /c"
+    randomizedResponseDelay(JSON.stringify(characters, null, 4), res);
+  } else if (req.url.length === 2) {
+    ("/a /b /c");
     const letterChosen = req.url[1].toLowerCase();
-    const allCharactersBeginningWith = characters.filter(char => {
-        return char[0].toLowerCase() === letterChosen;
+    const allCharactersBeginningWith = characters.filter((char) => {
+      return char[0].toLowerCase() === letterChosen;
     });
-    randomizedResponseDelay(JSON.stringify(allCharactersBeginningWith, null, 4), res);
+    randomizedResponseDelay(
+      JSON.stringify(allCharactersBeginningWith, null, 4),
+      res
+    );
+  } else if (req.url === "/hello") {
+    fs.appendFile("./greetings.txt", "hello");
+    res.write("Your hello has been recorded!");
+    res.end();
   } else {
+    res.writeHead(404);
     res.write(`
      Hello. 
      Thanks for contacting me! :) 
      You look good today!
+     But I dunno what you want?
     `);
     res.end();
   }
