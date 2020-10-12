910890af04ae6e1765ce9c599f3e30782cb14a20
Joe Alves <jmalves@appacademy.io>
2020-10-12 13:57:25 -0700

    basic server

---
+++ index.js
@@ -0,0 +1,13 @@
+const http = require("http");
+const server = http.createServer();
+
+server.on("request", (req, res) => {
+    res.write(`
+     Hello. 
+     Thanks for contacting me! :) 
+     You look good today!
+    `);
+    res.end();
+});
+
+server.listen(80);
